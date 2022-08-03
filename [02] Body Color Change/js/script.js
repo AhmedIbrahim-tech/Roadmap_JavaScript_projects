99ae465d294b131ab13f2b3a776d9f073407be7b
AhmedIbrahim-tech <ebrahema89859@gmail.com>
2022-08-03 21:16:05 +0200

    Add Change Color and Toggle Password

---
+++ [02] Body Color Change/js/script.js
@@ -0,0 +1,16 @@
+var buttonEl = document.querySelector(".btn");
+var divEl = document.querySelector(".mydiv");
+
+var colors = ["red", "green", "blue"];
+var i = 0;
+
+buttonEl.onclick = function() {
+    // logic
+
+    divEl.style.backgroundColor = colors[i]; // green
+    i++; // 3 
+
+    if(i == 3) {
+        i = 0
+    }
+}
